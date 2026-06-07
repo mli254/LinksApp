44722f385b008aa74b011b70c34cda7bfe1fba99
mli254 <mli254@ucsc.edu>
2026-06-07 07:08:09 -0700

    added readme information and additional comments

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # LinksApp
 
+## Description
+A simple React web application that provides information about Madison Li. It displays a profile image, a short bio, and four links to a resume, portfolio, GitHub, and LinkedIn. There is also a toggle for light/dark mode that will persist on refresh, and a small greeting that changes based on the time of day.
+
+## How to Run
+As a static webpage, running the program will require navigating to where the website is hosted at https://mli254.github.io/LinksApp/
+
+## JS Features
+* Dark Mode
+  * Using the button in the top left, users can swap between dark mode and light mode, with the theme they're currently on persisting even after reload. Additionally, there's a bit of an ease transition, so the change isn't too sudden.
+  * I picked this particular feature since I feel dark mode is almost universal, especially as it's essential for proper accessibility on websites—something which I wanted to keep in mind.
+  * What was tricky during implementation was learning about multiple different concepts: localStorage as a way to allow variables to persist across reloads, and React hooks like useState and useEffect as ways of creating responsive variables and accessing external systems respectively. I did use a reference for implementing dark mode, but I tried to understand every line of code that was written as well. One minor hurdle that cropped up was that the guide was written before Tailwind's v4 update which removed the need for a Tailwind config file, but luckily Tailwind's documentation showed the new method of allowing class to be toggled using JavaScript.
+  * With more time, I'd love to be able to sync the site's theme up with a user's own system preferences, while still allowing them to toggle between all three modes.
+* Time-of-day Greeting
+  * The time of day greeting will change it's message between "Good Morning/Afternoon/Evening" based on the time of day, which is also displayed in the sentence after.
+  * The time ranges are: 
+    * 12AM-11:59AM for Morning
+    * 12PM-6:59PM for Afternoon
+    * 7PM-11:59PM for Evening
+  * I chose this feature since it felt like a nice, personal touch. I wanted my application to have a bit of charm while remaining clean and professional, and I felt a small greeting that would update with the user's time would help achieve that affect, along with the 'cel-shaded' effect throughout my page's design.
+  * What was tricky when implementing it was primarily in understanding the Date object itself, as there were many helpful functions that altogether provided the functionality that I needed, but it was a matter of finding which functions were most helpful and integrating them into my code.
+  * With more time, I'd like to try and test it across different timezones, especially across different locales, as I formatted the string using the "en-us" locale only.
+## Issues
+* One persisting issue that I wasn't fully able to solve relates to the difference in formatting on mobile vs. computer. I tested my responsive sizing on both my computer (by resizing my browser) and my phone, but while the text on my computer wrapped normally in its box, the text on my phone—specifically for my LinkedIn link—overflowed. Even with additional CSS properties such as flex-shrink, which did solve the overflow issue, the way it displays still looks a bit odd, so I am a bit unsatisfied with that specific portion.
--- links-app/src/App.jsx
+++ links-app/src/App.jsx
@@ -17,9 +17,15 @@ function useDarkMode() {
   const [theme, setTheme] = useState(localStorage.theme || 'light');
 
   useEffect(() => {
+    // selects the root element in order to apply a 'global' property to the entire page
     const root = window.document.documentElement;
+    // sets true/false based on whether the theme is set to 'dark'
+    // '===' helps prevent some JavaScript shenanigans with comparisons
     const isDark = theme === 'dark';
 
+    // the toggle method accepts a token, and an optional 'force' argument
+    // a 'force' argument will turn the method into a one-way-only operation
+    // in this case, isDark = false will only remove 'dark', and isDark = true will only add 'dark'
     root.classList.toggle('dark', isDark);
     localStorage.setItem('theme', theme)
   }, [theme]);
@@ -27,6 +33,8 @@ function useDarkMode() {
   return {theme, setTheme}
 } 
 
+{/* Link Cards; abstracted enough to make adding new links easier */}
+{/* Because links open in new tabs, contains an aria-label to indicate what clicking on the card will do for accessibility reasons*/}
 function LinkCard({link}) {
   return (<>
     <div class="box box-border dark:border-[#D3F26A] border-1 rounded-xl p-5 mx-4 md:mx-8 bg-[#FBFFF5] dark:bg-[#54574F] shadow-[3px_3px_rgba(0,0,0)] dark:shadow-[3px_3px_#D3F26A] transition delay-80 duration-100 ease-in-out hover:shadow-none hover:translate-1 active:bg-[#C6CCC0]">
@@ -52,30 +60,50 @@ function LinkCard({link}) {
 function App() {
   const {theme, setTheme} = useDarkMode()
 
+  // finds current time of the user, and formats the date as hours:minutes AM/PM, excluding seconds
+  let current_time = new Date();
+  let greeting = "Evening"
+  let time_formatted = current_time.toLocaleTimeString("en-US", {timeStyle:'short'});
+
+  // modifies greeting based on time of day
+  let hours = current_time.getHours()
+  if (time_formatted.includes("AM")) {
+    greeting = "Morning"
+  } else if (hours > 11 && hours < 19) {
+    greeting = "Afternoon"
+  }
+
   return (
     <>
-      <div id="center_card" class="box box-border dark:border-[#D3F26A] border-2 rounded-xl w-100 md:w-150 mx-auto p-10 bg-[#D5DBC3] dark:bg-[#282924] shadow-[5px_5px_rgba(0,0,0)] dark:shadow-[5px_5px_#D3F26A]">
-        <div id="light_mode">
-          <button 
-            type="button" 
-            aria-label="Toggles theme between light or dark mode" 
-            onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
-            class="bg-[#FBFFF5] dark:bg-[#54574F] box-border dark:border-[#D3F26A] border-1 rounded-full p-2 shadow-[2px_3px_rgba(0,0,0)] dark:shadow-[3px_3px_#D3F26A] transition ease-in-out hover:shadow-none hover:translate-1 active:bg-[#C6CCC0]">
-            {theme=== 'dark' ? <FontAwesomeIcon icon={faMoon} size="lg"/> : <FontAwesomeIcon icon={faSun} size="lg"/>}
-            </button>
+    {/* Main center card; becomes thinner on smaller screens */}
+      <div class="box box-border dark:border-[#D3F26A] border-2 rounded-xl w-100 md:w-150 mx-auto p-10 bg-[#D5DBC3] dark:bg-[#282924] shadow-[5px_5px_rgba(0,0,0)] dark:shadow-[5px_5px_#D3F26A]">
+        {/* Dark/Light Mode toggle */}
+        <button 
+          type="button" 
+          aria-label="Toggles theme between light or dark mode" 
+          onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+          class="bg-[#FBFFF5] dark:bg-[#54574F] box-border dark:border-[#D3F26A] border-1 rounded-full p-2 shadow-[2px_3px_rgba(0,0,0)] dark:shadow-[3px_3px_#D3F26A] transition ease-in-out hover:shadow-none hover:translate-1 active:bg-[#C6CCC0]">
+          {theme === 'dark' ? <FontAwesomeIcon icon={faMoon} size="lg"/> : <FontAwesomeIcon icon={faSun} size="lg"/>}
+        </button>
+
+        {/* Time-of-day Greeting */}
+        <div class="text-center font-[Iosevka_Charon] m-4 bg-[#FBFFF5] dark:bg-[#54574F] rounded-lg p-1 w-30 md:w-50 text-sm md:text-base flex-initial mx-auto">
+          Good {greeting}! It is currently: {time_formatted}
         </div>
 
-        <div id="profile" class="text-center">
+        {/* Profile */}
+        <div class="text-center">
           <img src={profile_img} alt="image of Madison Li" class="rounded-full w-45 h-45 object-cover object-top mx-auto box-border dark:border-[#D3F26A] border-1 shadow-[2px_3px_rgba(0,0,0)] dark:shadow-[2px_3px_#D3F26A]"
           />
           <div class="p-5 mb-8">
             <h1 class="font-[Open_Sans] text-2xl font-bold">Madison Li</h1>
             <p class="font-[Akatab] text-lg text-black/50 dark:text-white/50">Software & Game Developer</p>
-            <p class="font-[Iosevka Charon] text-black/30 dark:text-white/30"><FontAwesomeIcon icon={faLocationDot} /> Sunnyvale, CA</p>
+            <p class="font-[Iosevka_Charon] text-black/30 dark:text-white/30"><FontAwesomeIcon icon={faLocationDot} /> Sunnyvale, CA</p>
             </div>
         </div>
 
-        <div id="links" class="flex flex-initial flex-col gap-y-8">
+        {/* Column of Links; iteratively creates link cards based on the objects in my 'links' array, making it easy to add more links in the future */}
+        <div class="flex flex-initial flex-col gap-y-8">
           {links.map((link) => <LinkCard link={link} key={link.id}/>)}
         </div>
       </div>
