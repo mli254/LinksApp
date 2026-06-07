de5abeb03aa76a0f607f971d263848cc7f4509ee
mli254 <mli254@ucsc.edu>
2026-06-07 05:20:15 -0700

    added dark mode toggle

--- links-app/src/App.jsx
+++ links-app/src/App.jsx
@@ -1,20 +1,35 @@
-// import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSun, faCode, faFilePdf, faLink, faLocationDot } from '@fortawesome/free-solid-svg-icons'
+import { faSun, faCode, faFilePdf, faLink, faLocationDot, faMoon } from '@fortawesome/free-solid-svg-icons'
 import {faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
 import profile_img from './assets/profile.jpg'
 import './App.css'
 
 const links = [
-  {title: "Resume", id: 1, icon: <FontAwesomeIcon icon={faFilePdf} size="xl"/>, link: "https://bit.ly/43l7pjy"},
-  {title: "Portfolio", id: 2, icon: <FontAwesomeIcon icon={faCode} size="xl"/>, link: "https://github.com/mli254"},
+  {title: "Resume", id: 1, icon: <FontAwesomeIcon icon={faFilePdf} size="xl"/>, link: "https://bit.ly/madison-li-resume"},
+  {title: "Portfolio", id: 2, icon: <FontAwesomeIcon icon={faCode} size="xl"/>, link: "https://bit.ly/madison-li-portfolio"},
   {title: "Github", id: 3, icon: <FontAwesomeIcon icon={faGithub} size="xl"/>, link: "https://github.com/mli254"},
   {title: "Linkedin", id: 4, icon: <FontAwesomeIcon icon={faLinkedin} size="xl"/>, link: "https://www.linkedin.com/in/limadison/"}
 ]
 
+// referenced from: https://dev.to/samanthalaine/implementing-dark-mode-in-a-react-app-3k1a
+function useDarkMode() {
+  const [theme, setTheme] = useState(localStorage.theme || 'light');
+
+  useEffect(() => {
+    const root = window.document.documentElement;
+    const isDark = theme === 'dark';
+
+    root.classList.toggle('dark', isDark);
+    localStorage.setItem('theme', theme)
+  }, [theme]);
+
+  return {theme, setTheme}
+} 
+
 function LinkCard({link}) {
   return (<>
-    <div class="box box-border border-1 rounded-xl p-5 mx-8 bg-[#FBFFF5] shadow-[3px_3px_rgba(0,0,0)] transition delay-80 duration-100 ease-in-out hover:shadow-none hover:translate-1 active:bg-[#C6CCC0]">
+    <div class="box box-border dark:border-[#D3F26A] border-1 rounded-xl p-5 mx-4 md:mx-8 bg-[#FBFFF5] dark:bg-[#54574F] shadow-[3px_3px_rgba(0,0,0)] dark:shadow-[3px_3px_#D3F26A] transition delay-80 duration-100 ease-in-out hover:shadow-none hover:translate-1 active:bg-[#C6CCC0]">
       <a 
       href={link.link} 
       target="_blank" 
@@ -26,7 +41,7 @@ function LinkCard({link}) {
         </div>
         <div>
           <h1 class="font-[Open_Sans] text-lg font-semibold">{link.title}</h1>
-          <h2 class="font-[Open_Sans] text-sm text-black/50"><FontAwesomeIcon icon={faLink} /> {link.link}</h2>
+          <h2 class="font-[Open_Sans] text-sm text-black/50 dark:text-white/50 text-wrap"><FontAwesomeIcon icon={faLink} /> {link.link}</h2>
           </div>
         </div>
       </a>
@@ -35,22 +50,28 @@ function LinkCard({link}) {
 }
 
 function App() {
+  const {theme, setTheme} = useDarkMode()
+
   return (
     <>
-      <div id="center_card" class="box box-border border-2 rounded-xl w-100 md:w-150 mx-auto p-10 bg-[#D5DBC3] shadow-[5px_5px_rgba(0,0,0)]">
+      <div id="center_card" class="box box-border dark:border-[#D3F26A] border-2 rounded-xl w-100 md:w-150 mx-auto p-10 bg-[#D5DBC3] dark:bg-[#282924] shadow-[5px_5px_rgba(0,0,0)] dark:shadow-[5px_5px_#D3F26A]">
         <div id="light_mode">
-          <button type="button" aria-label="Toggles between light or dark mode" class="bg-[#FBFFF5] box-border border-1 rounded-full p-2 shadow-[2px_3px_rgba(0,0,0)] transition delay-100 duration-100 ease-in-out hover:shadow-none hover:translate-1 active:bg-[#C6CCC0]">
-            <FontAwesomeIcon icon={faSun} size="lg"/>
+          <button 
+            type="button" 
+            aria-label="Toggles theme between light or dark mode" 
+            onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+            class="bg-[#FBFFF5] dark:bg-[#54574F] box-border dark:border-[#D3F26A] border-1 rounded-full p-2 shadow-[2px_3px_rgba(0,0,0)] dark:shadow-[3px_3px_#D3F26A] transition delay-100 duration-100 ease-in-out hover:shadow-none hover:translate-1 active:bg-[#C6CCC0]">
+            {theme=== 'dark' ? <FontAwesomeIcon icon={faMoon} size="lg"/> : <FontAwesomeIcon icon={faSun} size="lg"/>}
             </button>
         </div>
 
         <div id="profile" class="text-center">
-          <img src={profile_img} alt="image of Madison Li" class="rounded-full w-45 h-45 object-cover object-top mx-auto box-border border-1 shadow-[2px_3px_rgba(0,0,0)]"
+          <img src={profile_img} alt="image of Madison Li" class="rounded-full w-45 h-45 object-cover object-top mx-auto box-border dark:border-[#D3F26A] border-1 shadow-[2px_3px_rgba(0,0,0)] dark:shadow-[2px_3px_#D3F26A]"
           />
           <div class="p-5 mb-8">
             <h1 class="font-[Open_Sans] text-2xl font-bold">Madison Li</h1>
-            <p class="font-[Akatab] text-lg text-black/50">Software & Game Developer</p>
-            <p class="font-[Iosevka Charon] text-black/30"><FontAwesomeIcon icon={faLocationDot} /> Sunnyvale, CA</p>
+            <p class="font-[Akatab] text-lg text-black/50 dark:text-white/50">Software & Game Developer</p>
+            <p class="font-[Iosevka Charon] text-black/30 dark:text-white/30"><FontAwesomeIcon icon={faLocationDot} /> Sunnyvale, CA</p>
             </div>
         </div>
 
