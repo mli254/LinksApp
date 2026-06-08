43c75ae32a0ce70b1b071e7fcdd49cdeef41d3e9
mli254 <mli254@ucsc.edu>
2026-06-07 23:11:03 -0700

    tweaked sizing (shrunk layout overall)

--- links-app/src/App.jsx
+++ links-app/src/App.jsx
@@ -6,10 +6,10 @@ import profile_img from './assets/profile.jpg'
 import './App.css'
 
 const links = [
-  {title: "Resume", id: 1, icon: <FontAwesomeIcon icon={faFilePdf} size="xl"/>, link: "https://bit.ly/madison-li-resume"},
-  {title: "Portfolio", id: 2, icon: <FontAwesomeIcon icon={faCode} size="xl"/>, link: "https://bit.ly/madison-li-portfolio"},
-  {title: "Github", id: 3, icon: <FontAwesomeIcon icon={faGithub} size="xl"/>, link: "https://github.com/mli254"},
-  {title: "Linkedin", id: 4, icon: <FontAwesomeIcon icon={faLinkedin} size="xl"/>, link: "https://www.linkedin.com/in/limadison/"}
+  {title: "Resume", id: 1, icon: <FontAwesomeIcon icon={faFilePdf} size="lg"/>, link: "https://bit.ly/madison-li-resume"},
+  {title: "Portfolio", id: 2, icon: <FontAwesomeIcon icon={faCode} size="lg"/>, link: "https://bit.ly/madison-li-portfolio"},
+  {title: "Github", id: 3, icon: <FontAwesomeIcon icon={faGithub} size="lg"/>, link: "https://github.com/mli254"},
+  {title: "Linkedin", id: 4, icon: <FontAwesomeIcon icon={faLinkedin} size="lg"/>, link: "https://www.linkedin.com/in/limadison/"}
 ]
 
 // referenced from: https://dev.to/samanthalaine/implementing-dark-mode-in-a-react-app-3k1a
@@ -39,19 +39,19 @@ function LinkCard({link}) {
   return (<>
     <div 
       aria-label="Madison's Links"
-      class="box box-border border-black dark:border-[#D3F26A] border-1 rounded-xl p-5 mx-4 md:mx-8 bg-[#FBFFF5] dark:bg-[#54574F] shadow-[3px_3px_rgba(0,0,0)] dark:shadow-[3px_3px_#D3F26A] transition delay-80 duration-100 ease-in-out hover:shadow-none hover:translate-1 active:bg-[#C6CCC0]">
+      class="box box-border border-black dark:border-[#D3F26A] border-1 rounded-lg p-4 md:px-5 md:py-4 mx-2 md:mx-6 bg-[#FBFFF5] dark:bg-[#54574F] shadow-[2px_2px_rgba(0,0,0)] dark:shadow-[2px_2px_#D3F26A] transition delay-80 duration-100 ease-in-out hover:shadow-none hover:translate-1 active:bg-[#C6CCC0]">
       <a 
       href={link.link} 
       target="_blank" 
       aria-labelledby={`Opens a link to Madison's ${link.title} in new tab`}
       rel="noreferrer noopener">
         <div class="flex flex-row gap-x-2 items-center">
-          <div>
+          <div class="text-sm md:text-base">
           {link.icon}
-        </div>
+          </div>
         <div>
-          <h1 class="font-[Open_Sans] text-sm md:text-lg font-semibold">{link.title}</h1>
-          <h2 class="font-[Open_Sans] text-xs md:text-sm text-black/50 dark:text-white/50 shrink gap-x-1 items-center"><FontAwesomeIcon icon={faLink} /> {link.link}</h2>
+          <h1 class="font-[Open_Sans] text-xs md:text-sm font-semibold">{link.title}</h1>
+          <h2 class="font-[Open_Sans] text-[0.60rem] md:text-xs text-black/50 dark:text-white/50 shrink gap-x-1 items-center"><FontAwesomeIcon icon={faLink} /> {link.link}</h2>
           </div>
         </div>
       </a>
@@ -80,34 +80,34 @@ function App() {
     {/* Main center card; becomes thinner on smaller screens */}
       <div 
         aria-label="central-card"
-        class="box box-border border-black dark:border-[#D3F26A] border-2 rounded-xl w-100 md:w-150 mx-auto p-10 bg-[#D5DBC3] dark:bg-[#282924] shadow-[5px_5px_rgba(0,0,0)] dark:shadow-[5px_5px_#D3F26A]">
+        class="box box-border border-black dark:border-[#D3F26A] border-2 rounded-lg w-75 md:w-110 mx-auto p-5 bg-[#D5DBC3] dark:bg-[#282924] shadow-[4px_4px_rgba(0,0,0)] dark:shadow-[4px_4px_#D3F26A]">
         {/* Dark/Light Mode toggle */}
         <button 
           type="button" 
           aria-label="Toggles theme between light or dark mode" 
           onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
-          class="bg-[#FBFFF5] dark:bg-[#54574F] box-border border-black dark:border-[#D3F26A] border-1 rounded-full p-2 shadow-[2px_3px_rgba(0,0,0)] dark:shadow-[3px_3px_#D3F26A] transition ease-in-out hover:shadow-none hover:translate-1 active:bg-[#C6CCC0]">
-          {theme === 'dark' ? <FontAwesomeIcon icon={faMoon} size="lg"/> : <FontAwesomeIcon icon={faSun} size="lg"/>}
+          class="bg-[#FBFFF5] dark:bg-[#54574F] box-border border-black dark:border-[#D3F26A] border-1 rounded-full w-8 h-8 items-center m-2 shadow-[1px_2px_rgba(0,0,0)] dark:shadow-[1px_2px_#D3F26A] transition ease-in-out hover:shadow-none hover:translate-1 active:bg-[#C6CCC0]">
+          {theme === 'dark' ? <FontAwesomeIcon icon={faMoon} size="sm"/> : <FontAwesomeIcon icon={faSun} size="sm"/>}
         </button>
 
         {/* Time-of-day Greeting */}
-        <div class="text-center font-[Iosevka_Charon] m-4 bg-[#FBFFF5] dark:bg-[#54574F] rounded-lg p-1 w-30 md:w-50 text-sm md:text-base flex-initial mx-auto">
+        <div class="text-center font-[Iosevka_Charon] mx-4 mb-4 bg-[#FBFFF5] dark:bg-[#54574F] rounded-md p-1 w-25 md:w-35 text-xs md:text-xs flex-initial mx-auto">
           Good {greeting}! It is currently: {time_formatted}
         </div>
 
         {/* Profile */}
         <div class="text-center">
-          <img src={profile_img} alt="image of Madison Li" class="rounded-full w-45 h-45 object-cover object-top mx-auto box-border border-black dark:border-[#D3F26A] border-1 shadow-[2px_3px_rgba(0,0,0)] dark:shadow-[2px_3px_#D3F26A]"
+          <img src={profile_img} alt="image of Madison Li" class="rounded-full w-30 h-30 md:w-35 md:h-35 object-cover object-top mx-auto box-border border-black dark:border-[#D3F26A] border-1 shadow-[1px_2px_rgba(0,0,0)] dark:shadow-[1px_2px_#D3F26A]"
           />
-          <div class="p-5 mb-8">
-            <h1 class="font-[Open_Sans] text-2xl font-bold">Madison Li</h1>
-            <p class="font-[Akatab] text-lg text-black/50 dark:text-white/50">Software & Game Developer</p>
-            <p class="font-[Iosevka_Charon] text-black/40 dark:text-white/40"><FontAwesomeIcon icon={faLocationDot} /> Sunnyvale, CA</p>
+          <div class="p-3 mb-6">
+            <h1 class="font-[Open_Sans] text-[1.10rem] md:text-lg font-bold">Madison Li</h1>
+            <p class="font-[Akatab] text-[0.80rem] md:text-sm text-black/50 dark:text-white/50">Software & Game Developer</p>
+            <p class="font-[Iosevka_Charon] text-xs text-black/40 dark:text-white/40"><FontAwesomeIcon icon={faLocationDot} /> Sunnyvale, CA</p>
             </div>
         </div>
 
         {/* Column of Links; iteratively creates link cards based on the objects in my 'links' array, making it easy to add more links in the future */}
-        <div class="flex flex-initial flex-col gap-y-8">
+        <div class="flex flex-initial flex-col gap-y-5 mb-4">
           {links.map((link) => <LinkCard link={link} key={link.id}/>)}
         </div>
       </div>
